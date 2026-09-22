6b13c33d26676ea3e650b5663ef82d90b3c71e50
Anup Chaudhary <74030059+Anup10430@users.noreply.github.com>
2026-09-22 20:33:37 +0545

    Feat: Adding Html

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ const app = express();
 
 app.use(express.urlencoded());
 app.use(userRouter);
-app.use(hostRouter);
+app.use("/host", hostRouter);
+
+app.use((req, res, next) => {
+    res.status(404).send("<h1>404 Not Found</h1>");
+})
 
 const PORT = 3000;
 app.listen(PORT, () => {
